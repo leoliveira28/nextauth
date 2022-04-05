e746a5b7253df7558abc0833ef1f939adee27bb8
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-04-05 06:24:51 -0300

    refresh token

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -31,7 +31,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         const { 'nextauth.token': token} = parseCookies()
         if (token) {
             api.get('/me').then(response => {
-                const { email, permissions, roles } = response.data
+                const { email, permissions, roles } = response.data;
+                setUser({email, permissions, roles})
             })
         }
     }, [])
@@ -41,22 +42,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
                 email,
                 password,
             })
-            const { token, refreshToken, permissions, roles } = response.data
+            const { token, refreshToken, permissions, roles } = response.data;
             setCookie(undefined, 'nextauth.token', token, {
                 maxAge: 60 * 60 * 24 * 30, //30 days
                 path: '/'
-             } )
+             })
             setCookie(undefined, 'nextauth.refresh', refreshToken,  {
                 maxAge: 60 * 60 * 24 * 30, //30 days
                 path: '/'
-             }  )
+             })
 
             setUser({
                 email,
                 permissions,
                 roles,
             })
-            api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
             Router.push('/dashboard')
         } catch (err){
             console.log(err);
--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie  } from 'nookies' 
-import { resolve } from 'path';
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -14,45 +13,29 @@ export const api = axios.create({
 
 api.interceptors.response.use(response => {
     return response
-}, error => {
-    if (error.response.data?.code === 'token.expired') {
-        cookies = parseCookies();
+}, (error: AxiosError) => {
+    if(error.response.status === 401) {
+        if(error.response.data?.code === 'token.expired') {
+            cookies = parseCookies();
+            const { 'nextauth.refreshToken': refreshToken } = cookies;
+            api.post('/refresh', {
+                refreshToken,
+            }).then(response => {
+                const { token } = response.data;
 
-        const { 'nextauth.refreshToken' : refreshToken} = cookies
-        const originalConfig = error.config
-      if (!isRefreshing) {
-          isRefreshing = true;
-        api.post('/refresh', { 
-            refreshToken,
-        }).then(response => {
-            const { token } = response.data;
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 60 * 60 * 24 * 30, //30 days
+                    path: '/'
+                 })
+                setCookie(undefined, 'nextauth.refresh', response.data.refreshToken,  {
+                    maxAge: 60 * 60 * 24 * 30, //30 days
+                    path: '/'
+                 })
+                 api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
-            setCookie(undefined, 'nextauth.token', token, {
-                maxAge: 60 * 60 * 24 * 30, //30 days
-                path: '/'
-             } )
-            setCookie(undefined, 'nextauth.refresh', response.data.refreshToken,  {
-                maxAge: 60 * 60 * 24 * 30, //30 days
-                path: '/'
-             }  )
-             api.defaults.headers['Authorization'] = `Bearer ${token}`;
-        });
-    } 
-    return new Promise((resolve, reject) => {
-        failedRequestQueue.push({
-            resolve: (token: string) => {
-                if (!originalConfig?.headers) {
-                    return
-                }
-                originalConfig.headers['Authorization'] `Bearer ${token}`
-                resolve(api(originalConfig))
-            } ,
-            reject: (err: AxiosError) => {
-                reject(err)
-            },
-        })
-    })
-      } else {
-
+            });
+        } else {
+            // deslogar usuario
+        }
     }
-})
+});
